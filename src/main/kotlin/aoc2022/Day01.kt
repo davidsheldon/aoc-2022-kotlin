ea582380daf9013f60ed45bf23b92611cf871824
David Sheldon <davsheld@thousandeyes.com>
2022-12-01 13:16:23 +0000

    Day1 + setup.

---
+++ src/main/kotlin/aoc2022/Day01.kt
@@ -0,0 +1,84 @@
+package aoc2022
+
+import aoc2022.utils.InputUtils
+
+fun main() {
+    val testInput = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000""".split("\n")
+
+    fun <T> Sequence<T>.splitBy(predicate: (T) -> Boolean): Sequence<List<T>> {
+        val seq = this;
+        if (!seq.iterator().hasNext())
+            return emptySequence()
+        return sequence {
+            val current = mutableListOf<T>()
+            seq.forEach { element ->
+                val split = predicate(element)
+                if (split && current.isNotEmpty()) {
+                    yield(current)
+                    current.clear()
+                }
+                else {
+                    current.add(element)
+                }
+            }
+            if (current.isNotEmpty()) yield(current)
+        }
+    }
+
+
+    fun elfCalories1(input: List<String>) =
+        input.asSequence().splitBy(String::isBlank)
+            .map { calList -> calList.map(String::toInt).sum() }
+
+    fun Sequence<String>.elfCaloriesInternal() = sequence {
+        var currentElf = 0
+        forEach {
+            if (it.isBlank()) {
+                yield(currentElf)
+                currentElf = 0
+            }
+            else {
+                currentElf += it.toInt()
+            }
+        }
+    }
+
+    fun elfCalories(input: List<String>) = input.asSequence().elfCaloriesInternal()
+
+
+    fun part1(input: List<String>): Int {
+        return elfCalories(input)
+            .max()
+    }
+
+    fun part2(input: List<String>): Int {
+        return elfCalories(input)
+            .sortedDescending()
+            .take(3)
+            .sum()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testValue = part1(testInput)
+    println(testValue)
+    check(testValue == 24000)
+
+    val puzzleInput = InputUtils.downloadAndGetLines(2022, 1)
+    val input = puzzleInput.toList()
+
+    println(part1(input))
+    println(part2(input))
+}
---
+++ src/main/kotlin/aoc2022/utils/InputUtils.kt
@@ -0,0 +1,75 @@
+package aoc2022.utils
+
+import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.*
+
+object InputUtils {
+
+    private val inputPath: Path by lazy {
+        val root = Paths.get("").toAbsolutePath().toString()
+        Paths.get(root, "src/main/resources")
+    }
+
+    private val secrets: Map<String, String> by lazy {
+        this.javaClass.classLoader.getResourceAsStream("secret.properties").use { secrets ->
+            val properties = Properties()
+            properties.load(secrets)
+            properties.toMap()
+        } as Map<String, String>
+    }
+
+    private fun resourcePath(year: Int, day: Int): Path {
+        return Paths.get(inputPath.toAbsolutePath().toString(), "y$year", "day$day.txt")
+    }
+
+    private fun getStream(year: Int, day: Int): InputStream {
+        return Files.newInputStream(resourcePath(year, day))
+    }
+
+    fun getStream(fileName: String): InputStream = this.javaClass.classLoader.getResourceAsStream(fileName)
+
+    fun getString(fileName: String): String = getStream(fileName).bufferedReader().use { it.readText() }
+
+    private fun getStream(year: String, name: String): InputStream =
+        this.javaClass.classLoader.getResourceAsStream("y$year/$name")
+
+    fun getString(year: String, name: String): String = getStream(year, name).bufferedReader().use { it.readText() }
+
+    fun getString(year: Int, day: Int): String = getStream(year, day).bufferedReader().use { it.readText() }
+
+    fun getLines(year: String, name: String): Sequence<String> =
+        getStream(year, name).bufferedReader().use { it.readLines().asSequence() }
+
+    fun getLines(year: Int, day: Int): Sequence<String> =
+        getStream(year, day).bufferedReader().use { it.readLines().asSequence() }
+
+    fun downloadAndGetLines(year: Int, day: Int): Sequence<String> {
+        downloadInputIfNeeded(year, day)
+        return getLines(year, day)
+    }
+
+    fun downloadAndGetString(year: Int, day: Int): String {
+        downloadInputIfNeeded(year, day)
+        return getString(year, day)
+    }
+
+    private fun downloadInputIfNeeded(year: Int, day: Int) {
+        val resourcePath = resourcePath(year, day)
+        if (!Files.exists(resourcePath)) {
+            println("Input for y$year d$day doesn't exist. Downloading from adventofcode.com")
+            //https://adventofcode.com/2021/day/1/input3
+            val url = "https://adventofcode.com/$year/day/$day/input"
+            val cookies = secrets.filterKeys { it == "session" }
+            DownloadUtils.downloadFile(url, cookies).use { input ->
+                resourcePath.toFile().outputStream().use { output ->
+                    val bytes = input.transferTo(output)
+                    println("Downloaded $bytes bytes to ${resourcePath.toAbsolutePath()}")
+                }
+            }
+        }
+    }
+
+}
