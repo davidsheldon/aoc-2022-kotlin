1dc7a65cc81d6ded3e968f7f6bc27f476f4429e7
David Sheldon <dave@earth.li>
2022-12-06 06:34:16 +0000

    2021 Day3

--- src/main/kotlin/aoc2021/Day03.kt
+++ src/main/kotlin/aoc2021/Day03.kt
@@ -17,17 +17,17 @@ fun main() {
 00010
 01010""".split("\n")
 
-    fun <K> Map<K, Int>.topKeyByValues() = this.keys.sortedByDescending { this[it] }.first()
+    fun <K> Map<K, Int>.topKeyByValues() = if (values.size == values.toSet().size) {
+        this.keys.sortedByDescending { this[it] }.first()
+    } else {
+        null
+    }
 
     fun <T> Iterable<T>.mostFrequent() = groupingBy { it }.eachCount().topKeyByValues()
 
-    fun swapBits(gamma: String) = gamma.map {
-        when (it) {
-            '0' -> '1'
-            '1' -> '0'
-            else -> it
-        }
-    }.joinToString("")
+    fun not(c: Char): Char = ('1' + '0'.code - c).toChar()
+
+    fun swapBits(gamma: String) = gamma.map { not(it) }.joinToString("")
 
     fun part1(input: List<String>): Int {
         val gamma = input.asSequence()
@@ -44,11 +44,24 @@ fun main() {
         return gamma.toInt(2) * epsilon.toInt(2)
     }
 
-    fun filtered(digit: Int, boolean: Boolean, items: List<String>) {
-        items.map { it[digit] }.mostFrequent()
+    fun filtered(digit: Int, boolean: Boolean, items: List<String>): String {
+        val digits = items.map { it[digit] }
+        val mostFrequent = digits.mostFrequent() ?: '1'
+        val toMatch = if (boolean) mostFrequent else not(mostFrequent)
+        val matched = items.filter { it[digit] == toMatch }
+        return if (matched.size == 1) matched.first()
+        else filtered(digit + 1, boolean, matched)
     }
 
-    fun part2(input: List<String>): Int = part1(input) // TODO
+    fun part2(input: List<String>): Int {
+        val oxygenBits = filtered(0, true, input)
+        val co2Bits = filtered(0, false, input)
+
+        println(oxygenBits)
+        println(co2Bits)
+        return oxygenBits.toInt(2) * co2Bits.toInt(2)
+
+    }
 
     // test if implementation meets criteria from the description, like:
     val testValue = part1(testInput)
