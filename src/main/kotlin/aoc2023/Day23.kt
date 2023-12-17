e486dc801878bf8c0d61a5fdf2f9c8efabdd2ad0
David Sheldon <dave@earth.li>
2023-12-17 06:34:23 +0000

    Templates for remaining days

---
+++ src/main/kotlin/aoc2023/Day23.kt
@@ -0,0 +1,36 @@
+package aoc2023
+
+import utils.InputUtils
+
+
+fun main() {
+    val testInput = """
+        
+    """.trimIndent().split("\n")
+
+
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testValue = part1(testInput)
+    println(testValue)
+    check(testValue == -1)
+
+    println(part2(testInput))
+
+    val puzzleInput = InputUtils.downloadAndGetLines(2023, 23)
+    val input = puzzleInput.toList()
+
+    println(part1(input))
+    val start = System.currentTimeMillis()
+    println(part2(input))
+    println("Time: ${System.currentTimeMillis() - start}")
+}
